496e5fbe1ea6913bb51cc621ba37ecb8637935f3
Sergius92739 <sergem279@gmail.com>
2022-08-12 22:41:57 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import Form from './components/Form/Form';
 function App() {
   return (
     <div className="container">
+      <div className="link">
+        <a href="https://fakestoreapi.com/products?limit=5" target="blanc">Фейковые данные о товарах</a>, взяты с сайта
+        <a href='https://fakestoreapi.com/' target='blanc'> fakestoreapi.com</a>
+      </div>
       <Form />
       <CardList />
     </div>
